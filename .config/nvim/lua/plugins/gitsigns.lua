f1499ec8c96e4a01e97275a751000036f9ccac33
Sage Baram <sagebaram@gamil.com>
2021-08-08 14:45:44 +0300

    refactor(nvim): change gutter signs to '|' with different colors

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -5,11 +5,11 @@ end
 
 gitsigns.setup {
     signs = {
-        add = {hl = "DiffAdd", text = "+", numhl = "GitSignsAddNr"},
-        change = {hl = "DiffChange", text = "~", numhl = "GitSignsChangeNr"},
-        delete = {hl = "DiffDelete", text = "_", numhl = "GitSignsDeleteNr"},
-        topdelete = {hl = "DiffDelete", text = "‾", numhl = "GitSignsDeleteNr"},
-        changedelete = {hl = "DiffChange", text = "-", numhl = "GitSignsChangeNr"}
+        add = {hl = "DiffAdd", text = "|", numhl = "GitSignsAddNr"},
+        change = {hl = "DiffChange", text = "|", numhl = "GitSignsChangeNr"},
+        delete = {hl = "DiffDelete", text = "|", numhl = "GitSignsDeleteNr"},
+        topdelete = {hl = "DiffDelete", text = "|", numhl = "GitSignsDeleteNr"},
+        changedelete = {hl = "DiffChange", text = "|", numhl = "GitSignsChangeNr"}
     },
     numhl = false,
     keymaps = {

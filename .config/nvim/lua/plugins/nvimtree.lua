eca9f9e2be9de522e7d9a997b5a1f52dc33179cb
Sage Baram <sagebaram@gamil.com>
2021-08-08 14:52:06 +0300

    feat(nvim): hide statusline when nvim-tree is open

--- .config/nvim/lua/plugins/nvimtree.lua
+++ .config/nvim/lua/plugins/nvimtree.lua
@@ -2,7 +2,6 @@ local present, tree_c = pcall(require, "nvim-tree.config")
 if not present then
     return
 end
-
 local tree_cb = tree_c.nvim_tree_callback
 local g = vim.g
 
@@ -87,3 +86,6 @@ g.nvim_tree_bindings = {
     {key = "q", cb = tree_cb("close")},
     {key = "g?", cb = tree_cb("toggle_help")}
 }
+
+-- hide statusline when nvim tree is opened
+vim.cmd [[au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree" | set laststatus=0 | else | set laststatus=2 | endif]]

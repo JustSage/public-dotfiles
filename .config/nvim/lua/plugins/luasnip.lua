a75bbd5e17447097ad3f82a5d2b1d9ccfc17c73e
Sage Baram <sagebaram@gamil.com>
2021-08-14 23:30:21 +0300

    feat(nvim): add luasnip smart tab functions

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -3,11 +3,72 @@ if not present then
     return
 end
 
+local t = function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+    local col = vim.fn.col(".") - 1
+    if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+        return true
+    else
+        return false
+    end
+end
+
+_G.tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-n>"
+    elseif require("luasnip").expand_or_jumpable() then
+        return t "<cmd>lua require'luasnip'.jump(1)<Cr>"
+    elseif check_back_space() then
+        return t "<Tab>"
+    else
+        return vim.fn["compe#complete"]()
+    end
+end
+
+_G.s_tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-p>"
+    elseif require("luasnip").jumpable(-1) then
+        return t "<cmd>lua require'luasnip'.jump(-1)<CR>"
+    else
+        return t "<S-Tab>"
+    end
+end
+
+_G.completions = function()
+    local npairs
+    if
+        not pcall(
+            function()
+                npairs = require "nvim-autopairs"
+            end
+        )
+     then
+        return
+    end
+
+    if vim.fn.pumvisible() == 1 then
+        if vim.fn.complete_info()["selected"] ~= -1 then
+            return vim.fn["compe#confirm"]("<CR>")
+        end
+    end
+    return npairs.check_break_line_char()
+end
+
+--  compe mappings
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<CR>", "v:lua.completions()", {expr = true})
+
 luasnip.config.set_config(
     {
         history = true,
         updateevents = "TextChanged,TextChangedI"
-        -- updateevents = "InsertLeave"
     }
 )
 require("luasnip/loaders/from_vscode").load()

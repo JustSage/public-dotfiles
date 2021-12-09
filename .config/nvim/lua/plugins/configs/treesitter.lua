07cf5d15150edafa8ab36d7358e8d15e70bdc1ea
Sage Baram <sagebaram@gamil.com>
2021-12-09 20:07:05 +0200

    feat(treesitter): add org mode setup to treesitter

--- .config/nvim/lua/plugins/configs/treesitter.lua
+++ .config/nvim/lua/plugins/configs/treesitter.lua
@@ -4,28 +4,31 @@ if not present then
 end
 
 ts_config.setup {
-  highlight = {
-      enable = true,
-      use_languagetree = true
-  },
-  ensure_installed = {
-    "bash",
-    "regex",
-    "python",
-    "java",
-    "lua",
-    "cpp",
-    "css",
-    "scss",
-    "html",
-    "javascript",
-    "tsx",
-    "toml",
-    "json",
-    "c"
+    highlight = {
+        enable = true,
+        use_languagetree = true,
+    disable = {'org'}, -- Remove this to use TS highlighter for some of the highlights (Experimental)
+    additional_vim_regex_highlighting = {'org'}, -- Required since TS highlighter doesn't support all syntax features (conceal)
     },
-  rainbow = {
-      enable = true
-  }
+    ensure_installed = {
+        "org",
+        "bash",
+        "regex",
+        "python",
+        "java",
+        "lua",
+        "cpp",
+        "css",
+        "scss",
+        "html",
+        "javascript",
+        "tsx",
+        "typescript",
+        "toml",
+        "json",
+        "c",
+    },
+    rainbow = {
+        enable = true
+    }
 }
-

a4bb2dcf20003e363ed291d54cd2662d633e152e
Sage Baram <sagebaram@gamil.com>
2021-12-12 17:48:01 +0200

    refactor(map): restructure, add nvim-dap mappings, separate commands from mappings file

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -7,81 +7,96 @@ local function map(mode, lhs, rhs, opts) -- keybinding convention
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-local opt = {}
+-- local opt = {}
 
 
+-- Disable command history list on q:
+map("n", "q:", "<nop>")
+map("n", "Q", "<nop>")
 
-map("n", "<Leader>a", ":argadd <c-r>=fnameescape(expand('%:p:h'))<cr>/*<c-d>", opt)
-vim.cmd([[cnoremap <expr> / wildmenumode() ? "\<C-Y>" : "/"]])
-vim.cmd([[cnoremap <expr> * getcmdline() =~ '.*\*\*$' ? '/*' : '*']])
-vim.cmd([[cnoreabbr <expr> %% fnameescape(expand('%:p'))]])
+-- Add argument to namespace
+map("n", "<leader>a", ":argadd <c-r>=fnameescape(expand('%:p:h'))<cr>/*<c-d>")
 
-map("n", "Q", "<Nop>", opt)
+map("n", "<C-q>", ":lua require'core.functions'.toggle_quickfix()<CR>")
+map("n", "<C-.>", ":cnext<CR>zz")
+map("n", "<C-,>", ":cprev<CR>zz")
+map("n", "<C-;>", ":lnext<CR>zz")
+map("n", "<C-/>", ":lprev<CR>zz")
 
-map("n", "<C-q>", "<Cmd> lua require'functions'.toggle_quickfix()<CR>", opt)
-map("n", "<C-K>", "<Cmd> cnext<CR>zz", opt)
-map("n", "<C-J>", "<Cmd> cprev<CR>zz", opt)
-map("n", "<C-H>", "<Cmd> lnext<CR>zz", opt)
-map("n", "<C-L>", "<Cmd> lprev<CR>zz", opt)
+-- Better yank line
+-- map("n", "Y", "y$") -- nvim >= 0.6 built in
 
--- better yank line
-map("n", "Y", "y$", opt)
+-- Cursor standstill
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+map("n", "J", "mzJ`z")
 
--- cursor standstill
-map("n", "n", "nzzzv", opt)
-map("n", "N", "Nzzzv", opt)
-map("n", "J", "mzJ`z", opt)
+-- Moving lines faster with J & K
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+map("n", "<leader>j", ":m .+1<CR>==")
+map("n", "<leader>k", ":m .-2<CR>==")
 
---  moving lines faster with J & K
-map("v", "J", ":m '>+1<CR>gv=gv", opt)
-map("v", "K", ":m '<-2<CR>gv=gv", opt)
-map("n", "<Leader>j", ":m .+1<CR>==", opt)
-map("n", "<Leader>k", ":m .-2<CR>==", opt)
-
--- better undo
+-- Better undo - undos to punctuation
 map("i", ",", ",<c-g>u")
 map("i", ".", ".<c-g>u")
 map("i", "!", "!<c-g>u")
 map("i", "?", "?<c-g>u")
 
--- avoids loading packer at runtime.
-vim.cmd("silent! command PackerCompile lua require 'plugins.init' require('packer').compile()")
-vim.cmd("silent! command PackerInstall lua require 'plugins.init' require('packer').install()")
-vim.cmd("silent! command PackerStatus lua require 'plugins.init' require('packer').status()")
-vim.cmd("silent! command PackerSync lua require 'plugins.init' require('packer').sync()")
-vim.cmd("silent! command PackerUpdate lua require 'plugins.init' require('packer').update()")
+-- Use ESC to turn off search highlighting
+map("n", "<Esc>", ":noh<CR>")
 
-map("n", "<Leader>n", [[<Cmd> NvimTreeToggle<CR>]], opt) -- tree view
+-- Fugitive
+map("n", "<leader>gs", ":Git<CR>")
+map("n", "<leader>gf", ":diffget //2<CR>")
+map("n", "<leader>gj", ":diffget //3<CR>")
+map("n", "<leader>gb", ":Git blame<CR>")
 
--- use ESC to turn off search highlighting
-map("n", "<Esc>", ":noh<CR>", opt)
+-- Telescope
+map("n", "<leader>fg", ":Telescope live_grep<CR>")
+map("n", "<leader>gc", ":Telescope git_commits <CR>")
+map("n", "<leader>ff", ":Telescope find_files <CR>")
+map("n", "<leader>fb", ":Telescope buffers<CR>")
+map("n", "<leader>fh", ":Telescope help_tags<CR>")
+map("n", "<leader>fw", ":Telescope grep_string<CR>")
+map("n", "<leader>th", ":Telescope themes<CR>")
+map("n", "<C-g><C-g>", ":Telescope git_files <CR>")
 
--- git bindings
-map("n", "<Leader>gs", ":Git<CR>", opt)
-map("n", "<Leader>gf", ":diffget //2<CR>", opt)
-map("n", "<Leader>gj", ":diffget //3<CR>", opt)
-map("n", "<Leader>gb", ":Git blame<CR>", opt)
+map("n", "<leader>gq", ":Telescope ghq list<CR>")
+map("n", "<leader>rc", ":lua require('plugins.configs.telescope').search_dotfiles()<CR>")
 
--- telescope mappings
-map("n", "<Leader>fw", ":Telescope live_grep<CR>", opt)
-map("n", "<Leader>gc", ":Telescope git_commits <CR>", opt)
-map("n", "<Leader>ff", ":Telescope find_files <CR>", opt)
-map("n", "<Leader>fb", ":Telescope buffers<CR>", opt)
-map("n", "<Leader>fh", ":Telescope help_tags<CR>", opt)
-map("n", "<Leader>fw", ":Telescope grep_string<CR>", opt)
-map("n", "<Leader>th", ":Telescope themes<CR>", opt)
-map("n", "<Leader>gq", ":Telescope ghq list bin=<CR>", opt)
-map("n", "<C-g><C-g>", ":Telescope git_files <CR>", opt)
+-- Nvim Tree
+map("n", "<leader>n", ":NvimTreeToggle<CR>")
 
-map("n", "<C-h>", [[<Cmd>lua require("tmux").move_left()<CR>]], opt)
-map("n", "<C-j>", [[<Cmd>lua require("tmux").move_bottom()<CR>]], opt)
-map("n", "<C-k>", [[<Cmd>lua require("tmux").move_top()<CR>]], opt)
-map("n", "<C-l>", [[<Cmd>lua require("tmux").move_right()<CR>]], opt)
+-- Tmux integration
+map("n", "<C-h>", ":lua require('tmux').move_left()<CR>")
+map("n", "<C-j>", ":lua require('tmux').move_bottom()<CR>")
+map("n", "<C-k>", ":lua require('tmux').move_top()<CR>")
+map("n", "<C-l>", ":lua require('tmux').move_right()<CR>")
 
-map("n", "M-h", [[<Cmd>lua require("tmux").resize_left()<CR>]], opt)
-map("n", "M-j", [[<Cmd>lua require("tmux").resize_bottom()<CR>]], opt)
-map("n", "M-k", [[<Cmd>lua require("tmux").resize_top()<CR>]], opt)
-map("n", "M-l", [[<Cmd>lua require("tmux").resize_right()<CR>]], opt)
+map("n", "M-h", ":lua require('tmux').resize_left()<CR>")
+map("n", "M-j", ":lua require('tmux').resize_bottom()<CR>")
+map("n", "M-k", ":lua require('tmux').resize_top()<CR>")
+map("n", "M-l", ":lua require('tmux').resize_right()<CR>")
 
-map("n", "<C-t>", "<Plug>(ultest-next-fail)", opt)
-map("n", "<C-T>", "<Plug>(ultest-prev-fail)", opt)
+-- Ultest
+map("n", "<leader>tn", "<Plug>(ultest-next-fail)")
+map("n", "<leader>tp", "<Plug>(ultest-prev-fail)")
+
+-- Nvim dap
+map("n", "<leader>dh", ":lua require'dap'.toggle_breakpoint()<CR>")
+map("n", "<leader>dhh", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+
+map("n", "<leader>dn", ":lua require'dap'.run_to_cursor()<CR>")
+map("n", "<leader>dk", ":lua require'dap'.up()<CR>")
+map("n", "<leader>dj", ":lua require'dap'.down()<CR>")
+map("n", "<leader>dt", ":lua require'dap'.terminate()<CR>")
+map("n", "<leader>dr", ":lua require'dap'.repl.toggle({}, 'vsplit')<CR><C-w>l")
+map("n", "<leader>de", ":lua require'dap'.set_exception_breakpoints({'all'})<CR>")
+map("n", "<leader>di", ":lua require'dap.ui.widgets'.hover()<CR>")
+map("n", "<leader>d?", ":lua require'dap.ui.widgets';widgets.centered_float(widgets.scopes)<CR>")
+
+map("n", "<leader>ds", ":lua require'dap'.step_out()<CR>")
+map("n", "<leader>dss", ":lua require'dap'.step_into()<CR>")
+map("n", "<leader>dg", ":lua require'dap'.step_over()<CR>")
+map("n", "<leader>dc", ":lua require'dap'.continue()<CR>")

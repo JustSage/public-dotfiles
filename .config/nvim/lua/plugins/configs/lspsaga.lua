da0079ec416d3b382a3a775eb1938500cbb62ebb
Sage Baram <sagebaram@gamil.com>
2023-01-29 12:56:20 +0200

    (feat) add lspsaga & nvimtree new configs (nvim-ui)

--- .config/nvim/lua/plugins/configs/lspsaga.lua
+++ .config/nvim/lua/plugins/configs/lspsaga.lua
@@ -8,10 +8,8 @@ saga.setup({
     ui = {
         -- currently only round theme
         theme = "round",
-        -- this option only work in neovim 0.9
-        title = true,
         -- border type can be single,double,rounded,solid,shadow.
-        border = "solid",
+        border = "rounded",
         winblend = 0,
         expand = "",
         collapse = "",
@@ -20,27 +18,13 @@ saga.setup({
         diagnostic = "🐞",
         incoming = " ",
         outgoing = " ",
-        colors = {
-            --float window normal background color
-            normal_bg = "#1d1536",
-            --title background color
-            title_bg = "#afd700",
-            red = "#e95678",
-            magenta = "#b33076",
-            orange = "#FF8700",
-            yellow = "#f7bb3b",
-            green = "#afd700",
-            cyan = "#36d0e0",
-            blue = "#61afef",
-            purple = "#CBA6F7",
-            white = "#d1d4cf",
-            black = "#1c1c19",
-        },
+        colors = require("catppuccin.groups.integrations.lsp_saga").custom_colors(),
+        kind = require("catppuccin.groups.integrations.lsp_saga").custom_kind(),
     },
-    -- ui = {
-    -- 	colors = require("catppuccin.groups.integrations.lsp_saga").custom_colors(),
-    -- 	kind = require("catppuccin.groups.integrations.lsp_saga").custom_kind(),
-    -- },
+    preview = {
+        lines_above = 0,
+        lines_below = 0,
+    },
     lightbulb = {
         cache_code_action = false,
         virtual_text = false,
@@ -70,6 +54,7 @@ keymap("v", "<leader>ca", "<Cmd>Lspsaga code_action<CR>", opts)
 keymap("n", "<leader>ca", "<Cmd>Lspsaga code_action<CR>", opts)
 
 keymap("n", "rn", "<Cmd>Lspsaga rename<CR>", opts)
+keymap("n", "<leader>rn", "<cmd>Lspsaga rename ++project<CR>")
 
 -- Peek Definition
 -- you can edit the definition file in this flaotwindow

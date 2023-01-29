2e3ed73dc5ddcf08dda0fc28ccce184ac8adae74
Sage Baram <sagebaram@gamil.com>
2023-01-29 12:22:32 +0200

    (ref) prefix set to C-b, escape-time fix delays (tmux)

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -17,4 +17,3 @@ alias fig='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias vimf='GIT_DIR=$HOME/.cfg GIT_WORK_TREE=$HOME nvim'
 alias yarn='yarn --use-yarnrc "$XDG_CONFIG_HOME/yarn/config"'
 alias createnv='python3 -m venv'
-#alias svim=XDG_CONFIG_HOME=~/.config/nvim-simple nvim

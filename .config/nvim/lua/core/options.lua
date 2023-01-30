9e34c431e41534d749f8afac8f7c7410bb2968d2
Sage Baram <sagebaram@gamil.com>
2023-01-30 17:16:05 +0200

    (fix) command to disable continous comments fixed (opts)

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -3,11 +3,11 @@ local g = vim.g -- a table to set global variables
 local o = vim.opt -- a table to set option variables
 local bo = vim.bo -- a table to set buffer specific option variables
 local wo = vim.wo -- a table to set window specific option variables
-local cmd = vim.cmd -- vim cmd api
 local env = vim.env -- vim environment api
 -----------------------------------------------------------------
 o.encoding = "utf-8" -- encoding settings.
 o.fileencodings = "utf-8" -- encoding buffer settings.
+o.spelllang = "en_us" -- set spelling to english
 o.completeopt = "menuone,noinsert,noselect" -- complete option menu.
 o.termguicolors = true -- use the terminal gui colors.
 o.hidden = true -- navigate to other buffers without saving current.
@@ -34,31 +34,27 @@ o.cmdheight = 1 -- command prompt height.
 o.scrolloff = 10 -- keep 'n' lines visible when scrolling.
 o.lazyredraw = true -- good performance settings.
 o.diffopt = "vertical" -- opens git diff in vertical split.
--- o.timeoutlen = 500
-o.timeoutlen = 250
+o.timeoutlen = 250 -- timeout before key refresh.
 o.updatetime = 250 -- update interval for gitsigns.
 o.keywordprg = ":help" -- open help with 'K'.
 o.clipboard = "" -- clipboard settings for my os (darwin)
 o.fillchars = { eob = " " } -- avoid trailing whitespace
-o.mouse = "a"
+o.mouse = "a" -- enable mouse
 
--- vim.opt.list = true
--- vim.opt.listchars:append "space:⋅"
--- vim.opt.listchars:append "eol"
+vim.cmd([[setlocal formatoptions-=cro]]) -- disable comment continouation in next line
 
-cmd([[setlocal formatoptions-=cro]]) -- disable comment continouation in next line
 o.path:append({ "**" }) -- file paths, searching and ignores
 o.wildignore:append({
-  "*/node_modules/*",
-  "*.pyc",
-  "*.DS_Store",
-  "*.jpg",
-  "*.bmp",
-  "*.gif",
-  "*.png",
-  "*.jpeg",
-  "versions/*",
-  "cache/*",
+	"*/node_modules/*",
+	"*.pyc",
+	"*.DS_Store",
+	"*.jpg",
+	"*.bmp",
+	"*.gif",
+	"*.png",
+	"*.jpeg",
+	"versions/*",
+	"cache/*",
 })
 o.suffixesadd:append({ ".html", ".js", ".es", ".jsx", ".json", ".css", ".sass", ".py", ".md", ".java", ".c", ".cpp" })
 
@@ -72,9 +68,9 @@ o.signcolumn = "yes"
 wo.wrap = false
 
 -- change directory upon opening buffers, windows etc.
-g.autochdir = true
--- providers
+-- g.autochdir = true
 
+-- providers
 g.python3_host_prog = "$PYENV_ROOT/shims/python"
 g.python2_host_prog = "/usr/bin/python2"
 
@@ -82,26 +78,26 @@ env.GIT_WORK_TREE = vim.fn.expand("$HOME")
 env.GIT_DIR = vim.fn.expand("$HOME/.cfg")
 
 local disabled_built_ins = {
-  "netrw",
-  "netrwPlugin",
-  "netrwSettings",
-  "netrwFileHandlers",
-  "gzip",
-  "zip",
-  "zipPlugin",
-  "tar",
-  "tarPlugin",
-  "getscript",
-  "getscriptPlugin",
-  "vimball",
-  "vimballPlugin",
-  "2html_plugin",
-  "logipat",
-  "rrhelper",
-  "spellfile_plugin",
-  "matchit",
+	"netrw",
+	"netrwPlugin",
+	"netrwSettings",
+	"netrwFileHandlers",
+	"gzip",
+	"zip",
+	"zipPlugin",
+	"tar",
+	"tarPlugin",
+	"getscript",
+	"getscriptPlugin",
+	"vimball",
+	"vimballPlugin",
+	"2html_plugin",
+	"logipat",
+	"rrhelper",
+	"spellfile_plugin",
+	"matchit",
 }
 
 for _, plugin in pairs(disabled_built_ins) do
-  vim.g["loaded_" .. plugin] = 1
+	vim.g["loaded_" .. plugin] = 1
 end

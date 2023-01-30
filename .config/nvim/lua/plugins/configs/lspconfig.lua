2d9d5c97c5030a957c96a4dc7dde8e4e9ac03458
Sage Baram <sagebaram@gamil.com>
2023-01-30 17:17:10 +0200

    (feat) add marksman lsp configurations for markdown files (lsp)

--- .config/nvim/lua/plugins/configs/lspconfig.lua
+++ .config/nvim/lua/plugins/configs/lspconfig.lua
@@ -3,223 +3,230 @@ local util = require("lspconfig/util")
 
 local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status then
-    return
+	return
 end
 
 local lspconfig_status, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status then
-    return
+	return
 end
 
 -- remaps on attach to buffer
 local function on_attach(client, bufnr)
-    client.server_capabilities.documentationFormattingProvider = false
-    client.server_capabilities.documentRangeFormattingProvider = false
+	client.server_capabilities.documentationFormattingProvider = false
+	client.server_capabilities.documentRangeFormattingProvider = false
 
-    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set("n", "<space>wl", function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    vim.keymap.set("n", "<space>gd", vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set("n", "<space>gd", vim.lsp.buf.type_definition, bufopts)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 capabilities.textDocument.completion.completionItem = {
-    documentationFormat = { "markdown", "plaintext" },
-    snippetSupport = true,
-    preselectSupport = true,
-    insertReplaceSupport = true,
-    labelDetailsSupport = true,
-    deprecatedSupport = true,
-    commitCharactersSupport = true,
-    tagSupport = { valueSet = { 1 } },
-    resolveSupport = {
-        properties = {
-            "documentation",
-            "detail",
-            "additionalTextEdits",
-        },
-    },
+	documentationFormat = { "markdown", "plaintext" },
+	snippetSupport = true,
+	preselectSupport = true,
+	insertReplaceSupport = true,
+	labelDetailsSupport = true,
+	deprecatedSupport = true,
+	commitCharactersSupport = true,
+	tagSupport = { valueSet = { 1 } },
+	resolveSupport = {
+		properties = {
+			"documentation",
+			"detail",
+			"additionalTextEdits",
+		},
+	},
 }
 
 -- replace the default lsp diagnostic symbols
 
 local signs = {
-    { name = "DiagnosticsSignError", text = "" },
-    { name = "DiagnosticsSignWarn", text = "" },
-    { name = "DiagnosticsSignHint", text = "" },
-    { name = "DiagnosticsSignInfo", text = "" },
+	{ name = "DiagnosticsSignError", text = "" },
+	{ name = "DiagnosticsSignWarn", text = "" },
+	{ name = "DiagnosticsSignHint", text = "" },
+	{ name = "DiagnosticsSignInfo", text = "" },
 }
 
 for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
 end
 
 -- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
 local config = {
-    virtual_text = {
-        prefix = "",
-        spacing = 0,
-    },
-    signs = { active = signs },
-    underline = true,
+	virtual_text = {
+		prefix = "",
+		spacing = 0,
+	},
+	signs = { active = signs },
+	underline = true,
 
-    -- set this to true if you want diagnostics to show in insert mode
-    update_in_insert = false,
+	-- set this to true if you want diagnostics to show in insert mode
+	update_in_insert = false,
 }
 
 vim.diagnostic.config(config)
 
-
 vim.lsp.handlers["workspace/diagnostic/refresh"] = function(_, _, ctx)
-    local ns = vim.lsp.diagnostic.get_namespace(ctx.client_id)
-    local bufnr = vim.api.nvim_get_current_buf()
-    vim.diagnostic.reset(ns, bufnr)
-    return true
+	local ns = vim.lsp.diagnostic.get_namespace(ctx.client_id)
+	local bufnr = vim.api.nvim_get_current_buf()
+	vim.diagnostic.reset(ns, bufnr)
+	return true
 end
 
 -- suppress error messages from lang servers
 vim.notify = function(msg, log_level)
-    if msg:match("exit code") then
-        return
-    end
-    if log_level == vim.log.levels.ERROR then
-        vim.api.nvim_err_writeln(msg)
-    else
-        vim.api.nvim_echo({ { msg } }, true, {})
-    end
+	if msg:match("exit code") then
+		return
+	end
+	if log_level == vim.log.levels.ERROR then
+		vim.api.nvim_err_writeln(msg)
+	else
+		vim.api.nvim_echo({ { msg } }, true, {})
+	end
 end
 
 mason_lspconfig.setup({
-    ensure_installed = { "pyright", "tsserver", "sumneko_lua", "tailwindcss" },
+	ensure_installed = { "pyright", "tsserver", "sumneko_lua", "tailwindcss" },
 })
 
 lspconfig.tsserver.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    root_dir = util.root_pattern("package.json", ".git") or vim.fn.expand("%p"),
-    filetype = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
-    cmd = { "typescript-language-server", "--stdio" },
+	on_attach = on_attach,
+	capabilities = capabilities,
+	root_dir = util.root_pattern("package.json", ".git") or vim.fn.expand("%p"),
+	filetype = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+	cmd = { "typescript-language-server", "--stdio" },
 })
 
 lspconfig.tailwindcss.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
+	on_attach = on_attach,
+	capabilities = capabilities,
 })
 
 lspconfig.html.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    cmd = { "vscode-html-language-server", "--stdio" },
-    filetype = { "html" },
-    single_file_support = true,
-    init_options = {
-        configurationSection = { "html", "css", "javascript" },
-        embeddedLanguages = {
-            css = true,
-            javascript = true,
-        },
-        provideFormatter = true,
-    },
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = { "vscode-html-language-server", "--stdio" },
+	filetype = { "html" },
+	single_file_support = true,
+	init_options = {
+		configurationSection = { "html", "css", "javascript" },
+		embeddedLanguages = {
+			css = true,
+			javascript = true,
+		},
+		provideFormatter = true,
+	},
 })
 
 lspconfig.emmet_ls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
-    init_options = {
-        html = {
-            options = {
-                -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-                ["bem.enabled"] = true,
-            },
-        },
-    },
+	on_attach = on_attach,
+	capabilities = capabilities,
+	filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
+	init_options = {
+		html = {
+			options = {
+				-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+				["bem.enabled"] = true,
+			},
+		},
+	},
 })
 
 lspconfig.cssls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    cmd = { "vscode-css-language-server", "--stdio" },
-    filetypes = { "css", "scss", "less" },
-    root_dir = util.root_pattern("package.json", ".git") or vim.fn.expand("%p"),
-    single_file_support = true,
-    settings = {
-        css = { validate = true },
-    },
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = { "vscode-css-language-server", "--stdio" },
+	filetypes = { "css", "scss", "less" },
+	root_dir = util.root_pattern("package.json", ".git") or vim.fn.expand("%p"),
+	single_file_support = true,
+	settings = {
+		css = { validate = true },
+	},
 })
 
 lspconfig.pyright.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetype = { "python" },
-    cmd = { "pyright-langserver", "--stdio" },
-    settings = {
-        python = {
-            analysis = {
-                autoSearchPaths = true,
-                diagnosticMode = "workspace",
-                useLibraryCodeForTypes = true,
-            },
-        },
-    },
-    root_dir = function(fname)
-        return util.root_pattern("src", ".git", "setup.py", "setup.cfg", "pyproject.toml", "requirements.txt")(fname)
-            or util.path.dirname(fname)
-    end,
+	on_attach = on_attach,
+	capabilities = capabilities,
+	filetype = { "python" },
+	cmd = { "pyright-langserver", "--stdio" },
+	settings = {
+		python = {
+			analysis = {
+				autoSearchPaths = true,
+				diagnosticMode = "workspace",
+				useLibraryCodeForTypes = true,
+			},
+		},
+	},
+	root_dir = function(fname)
+		return util.root_pattern("src", ".git", "setup.py", "setup.cfg", "pyproject.toml", "requirements.txt")(fname)
+			or util.path.dirname(fname)
+	end,
 })
 
-lspconfig.bashls.setup ({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    cmd = { "start", "bash-language-server" },
-    cmd_env = { GLOB_PATTERN = "*@(.sh|.inc|.bash|.command)" },
-    filetypes = { "sh" },
+lspconfig.bashls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = { "start", "bash-language-server" },
+	cmd_env = { GLOB_PATTERN = "*@(.sh|.inc|.bash|.command)" },
+	filetypes = { "sh" },
+})
+
+lspconfig.marksman.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	-- cmd = { "markdown", "server" },
+	filetypes = { "markdown" },
+	root_dir = util.root_pattern(".git", ".marksman.toml"),
 })
 
 lspconfig.sumneko_lua.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    root_dir = vim.F.if_nil(
-        util.root_pattern({
-            ".git",
-            ".stylua.toml",
-            "stylua.toml",
-            ".styluaignore",
-            "selene.toml",
-            ".selene.toml",
-        }),
-        vim.loop.cwd()
-    ),
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { "vim" },
-            },
-            workspace = {
-                library = {
-                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-                },
-                maxPreload = 100000,
-                preloadFileSize = 10000,
-            },
-            telemetry = {
-                enable = false,
-            },
-            completion = {
-                callSnippet = "Replace",
-            },
-        },
-    },
+	on_attach = on_attach,
+	capabilities = capabilities,
+	root_dir = vim.F.if_nil(
+		util.root_pattern({
+			".git",
+			".stylua.toml",
+			"stylua.toml",
+			".styluaignore",
+			"selene.toml",
+			".selene.toml",
+		}),
+		vim.loop.cwd()
+	),
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+				},
+				maxPreload = 100000,
+				preloadFileSize = 10000,
+			},
+			telemetry = {
+				enable = false,
+			},
+			completion = {
+				callSnippet = "Replace",
+			},
+		},
+	},
 })
